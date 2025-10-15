b19c7cb0e4ee956384d3d2aef9ffc434701ce7ff
Marius1code <kanzemarius2@gmail.com>
2025-10-16 00:41:34 +0100

    ajout de l'attibut role a la table (entite) inscription

--- coldmanager_db(1).sql
+++ coldmanager_db(1).sql
@@ -91,14 +91,17 @@ INSERT INTO `employe` (`id`, `nom`, `prenom`, `email`, `age`) VALUES
 --
 
 CREATE TABLE `inscription` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `mdp` varchar(255) NOT NULL,
+  `role` varchar(20) NOT NULL DEFAULT 'user',
   `date_inscription` datetime DEFAULT current_timestamp(),
   `date_derniere_connexion` datetime DEFAULT NULL,
-  `est_actif` tinyint(1) DEFAULT 1
+  `est_actif` tinyint(1) DEFAULT 1,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
