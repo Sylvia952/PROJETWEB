1657b0d63ea540ab9e03050d946d4637344cf388
Marius1code <kanzemarius2@gmail.com>
2025-10-17 11:23:19 +0100

    deconn

--- deconnexion.php
+++ deconnexion.php
@@ -2,6 +2,12 @@
 session_start();
 session_unset();
 session_destroy();
-header("Location: index.php");
+
+// Détection auto selon l'environnement
+if ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '127.0.0.1') {
+    header("Location: http://localhost/PROJETWEB/index.php");
+} else {
+    header("Location: /index.php"); // racine de ton site InfinityFree
+}
 exit();
 ?>
